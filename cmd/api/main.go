818a0973b7aebdca0876d0fe9c1f49f3ce09922f
LUCA MARCHIORI <luca.marchiori@hotmail.it>
2024-04-18 09:49:36 +0200

    Fix comments

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,16 +11,10 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// Declare a string containing the application version number. Later in the book we'll
-// generate this automatically at build time, but for now we'll just store the version
-// number as a hard-coded global constant.
+// Application version number
 const version = "1.0.0"
 
-// Define a config struct to hold all the configuration settings for our application.
-// For now, the only configuration settings will be the network port that we want the
-// server to listen on, and the name of the current operating environment for the
-// application (development, staging, production, etc.). We will read in these
-// configuration settings from command-line flags when the application starts.
+// Config struct
 type config struct {
 	port int
 	env  string
